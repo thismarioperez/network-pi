70c13d3a367d4562a4adfd63a4352d04a3a2b8fa
Mario Perez <mario@level2d.com>
2021-07-24 16:14:42 -0400

    More fixes for configuring pi to work with current network settings

--- networkUp.sh
+++ networkUp.sh
@@ -3,7 +3,7 @@ docker network create \
     --driver=macvlan \
     --gateway=192.168.1.1 \
     --subnet=192.168.1.1/24 \
-    --ip-range=192.168.1.6/32 \
+    --ip-range=192.168.1.5/32 \
     -o parent=eth0 \
     VLAN_pi-hole
 echo "VLAN_pi-hole network created"
