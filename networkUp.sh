676eba1dca722f9d0288d810dd8386e5fa1f3fb0
Mario Perez <mario@level2d.com>
2021-07-24 16:34:44 -0400

    Configure local network gateway and subnet with environment variables

--- networkUp.sh
+++ networkUp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker network create \
     --driver=macvlan \
-    --gateway=192.168.1.1 \
-    --subnet=192.168.1.1/24 \
+    --gateway=${LOCAL_NETWORK_GATEWAY} \
+    --subnet=${LOCAL_NETWORK_SUBNET} \
     --ip-range=${PIHOLE_STATIC_IP}/32 \
     -o parent=eth0 \
     VLAN_pi-hole
