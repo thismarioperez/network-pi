6d7b75ef3cfde7d19ed88007507c58654e8d2a83
Mario Perez <mario@level2d.com>
2021-07-24 16:32:25 -0400

    Configure pihole with environment variables

--- networkUp.sh
+++ networkUp.sh
@@ -3,7 +3,7 @@ docker network create \
     --driver=macvlan \
     --gateway=192.168.1.1 \
     --subnet=192.168.1.1/24 \
-    --ip-range=192.168.1.5/32 \
+    --ip-range=${PIHOLE_STATIC_IP}/32 \
     -o parent=eth0 \
     VLAN_pi-hole
 echo "VLAN_pi-hole network created"
